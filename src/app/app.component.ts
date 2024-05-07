f8dbd82d49f9bba20ffedaafdac55f032a3034c0
Pratham Mehra <rc.zestgeek@gmail.com>
2024-05-07 17:24:53 +0530

    add the shake detect functionality

---
+++ src/app/app.component.ts
@@ -0,0 +1,74 @@
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import Shake from 'shake.js';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  toggleModal: boolean = false
+  toggleState: boolean = true
+  counter: number = 1;
+  private shakeEvent: any;
+  private lastAcceleration: DeviceMotionEventAcceleration;
+  @ViewChild('dynamicModal', { static: true }) dynamicModal: ElementRef;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    if ('ondevicemotion' in window) {
+      // Browser supports the devicemotion event
+      console.log('Device motion events supported');
+    } else {
+      // Browser does not support the devicemotion event
+      console.log('Device motion events not supported');
+    }
+
+    this.shakeEvent = new Shake({ threshold: 15, timeout: 1000 });
+    this.shakeEvent.start();
+    window.addEventListener('shake', () => this.handleShakeEvent(), false);
+  }
+  handleShakeEvent() {
+    // Handle the shake event here
+    this.counter++;
+    alert('Shake detected!');
+  }
+  handleClick() {
+    alert("test")
+  }
+
+
+
+
+  // handleShakeEvent() {
+  //   // Your logic for handling the shake event
+  //   this.counter++;
+  //   console.log('Shake detected!');
+  // }
+  // ngOnDestroy() {
+  //   this.deviceMotion.clearWatchAcceleration();
+  // }
+
+
+  public handleModal() {
+    this.toggleModal = !this.toggleModal
+    console.log("check button is click", this.toggleModal)
+  }
+
+  public onToggleChange(event: any) {
+    this.toggleState = event
+    console.log("check")
+  }
+
+  ngOnDestroy() {
+
+    this.shakeEvent.stop();
+    window.removeEventListener('shake', this.handleShakeEvent.bind(this), false);
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CommonModule } from '@angular/common';
+import { HttpBackend, HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ButtonsModule, LibModalModule, ToggleModule } from 'nextsapien-component-lib';
+import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+export const httpLoaderFactory = (http: HttpBackend): TranslateHttpLoader => new TranslateHttpLoader(new HttpClient(http), './assets/i18n/', '.json');
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ButtonsModule,
+    LibModalModule,
+    ToggleModule,
+    IonicModule.forRoot(),
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpBackend],
+      },
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
